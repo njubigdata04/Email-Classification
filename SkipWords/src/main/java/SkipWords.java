0e169a78ef9b9005c321a466a73d7dc6c8fc7caf
CjhGm <cjh327@126.com>
2019-06-27 20:06:06 +0800

    Update SkipWords: use lucene 5.0.0 for java 7

--- SkipWords/src/main/java/SkipWords.java
+++ SkipWords/src/main/java/SkipWords.java
@@ -1,13 +1,17 @@
 import java.io.*;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.filecache.ClientDistributedCacheManager;
+import org.apache.hadoop.mapreduce.filecache.DistributedCache;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.*;
@@ -16,11 +20,13 @@ import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.analysis.core.StopFilter;
 
 import java.io.IOException;
+
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.lucene.analysis.util.CharArraySet;
 
 
 public class SkipWords {
@@ -29,17 +35,38 @@ public class SkipWords {
 
         private final Text one = new Text("1");
         private Text label = new Text();
-        private String[] sws = new String[]{"I", "you", "hate"};
+        private String[] sws;
         private Analyzer analyzer;
 
         @Override
         public void setup(Context context) throws IOException, InterruptedException {
+            /*
+            Configuration conf = context.getConfiguration();
+            Path path[] = DistributedCache.getLocalCacheFiles(conf);
+            FileSystem fs = FileSystem.getLocal(conf);
+            FSDataInputStream in = fs.open(path[0]);
+            Scanner scan = new Scanner(in);
+            ArrayList<String> arrayList = new ArrayList<String>();
+            while (scan.hasNext()) {
+                String s = scan.next();
+                System.out.println(Thread.currentThread().getName() + "扫描的内容:  " + s);
+                arrayList.add(s);
+            }
+            scan.close();
+            fs.close();
+            sws = new String[arrayList.size()];
+            for (int i = 0; i < arrayList.size(); i++) {
+                sws[i] = arrayList.get(i);
+            }
+            */
+            sws = new String[]{"I", "you"};
             analyzer = new MyStopAnalyzer(sws);
         }
 
         @Override
         protected void map(Object key, Text value, Mapper<Object, Text, Text, Text>.Context context)
                 throws IOException, InterruptedException {
+            System.out.println("in setup");
             String fileName = getInputSplitFileName(context.getInputSplit());
             TokenStream stream = null;
             stream = analyzer.tokenStream("renyi", new StringReader(value.toString()));
@@ -47,7 +74,8 @@ public class SkipWords {
             //在lucene 4 以上  要加入reset 和  end方法
             stream.reset();
             while (stream.incrementToken()) {
-                label.set(String.join(":", cta.toString(), fileName));
+                System.out.println(cta.toString());
+                label.set(cta.toString() + ":" + fileName);
                 context.write(label, one);
             }
             stream.end();
@@ -77,7 +105,7 @@ public class SkipWords {
 
     public static String[] toArrayByFileReader(String name) {
         // 使用ArrayList来存储每行读取到的字符串
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<String>();
         try {
             FileReader fr = new FileReader(name);
             BufferedReader bf = new BufferedReader(fr);
@@ -133,15 +161,15 @@ public class SkipWords {
 
     public static void main(String[] args) throws Exception {
         /*
-        //String[] str = new String[]{"I", "you", "hate"};
-        Analyzer analyzer = new MyStopAnalyzer(args[0]);
-        Analyzer analyzer1 = new StopAnalyzer();
+        String[] str = new String[]{"I", "you", "hate"};
+        Analyzer analyzer1 = new MyStopAnalyzer(str);
+        Analyzer analyzer2 = new StopAnalyzer();
         String txt = "i love you, i hate you";
         //自定义的停用词分词器
-        display(txt, analyzer);
+        display(txt, analyzer1);
         System.out.println("-----------------------");
         //默认的停用词分词器
-        display(txt, analyzer1);
+        display(txt, analyzer2);
         String str = "hello, i'm a boy, and i like playing basketball" ;
         String ztr = "你好，我是一个男孩，我喜欢打篮球" ;
         Analyzer a = new StandardAnalyzer() ;      //标准分词器
@@ -156,14 +184,15 @@ public class SkipWords {
         System. out.println( "-----------------------------");
         display(str,d) ;
         System. out.println( "-----------------------------");
-    }
-    */
+        */
 
         String inputPath = "./input";
+        String cachePath = "./cache";
         String outputPath = "./output";
-        if (args.length == 2) {
+        if (args.length == 3) {
             inputPath = args[0];
-            outputPath = args[1];
+            cachePath = args[1];
+            outputPath = args[2];
         }
         Configuration configuration = new Configuration();
         removeOutputFolder(configuration, outputPath);
@@ -178,6 +207,7 @@ public class SkipWords {
         job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
+        //DistributedCache.addCacheFile(new URI(cachePath), job.getConfiguration());
         FileInputFormat.addInputPath(job, new Path(inputPath));
         FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
@@ -192,24 +222,26 @@ public class SkipWords {
             fileSystem.delete(path, true);
         }
     }
-/*
+
     public static void display(String str, Analyzer a) {
         TokenStream stream = null;
         try {
             stream = a.tokenStream("renyi", new StringReader(str));
-            PositionIncrementAttribute pia = stream.addAttribute(PositionIncrementAttribute.class);  //保存位置
-            OffsetAttribute oa = stream.addAttribute(OffsetAttribute.class); //保存辞与词之间偏移量
+            //  PositionIncrementAttribute pia = stream.addAttribute(PositionIncrementAttribute.class);  //保存位置
+            // OffsetAttribute oa = stream.addAttribute(OffsetAttribute.class); //保存辞与词之间偏移量
             CharTermAttribute cta = stream.addAttribute(CharTermAttribute.class);//保存响应词汇
-            TypeAttribute ta = stream.addAttribute(TypeAttribute.class); //保存类型
+            // TypeAttribute ta = stream.addAttribute(TypeAttribute.class); //保存类型
             //在lucene 4 以上  要加入reset 和  end方法
             stream.reset();
             while (stream.incrementToken()) {
-                System.out.println(pia.getPositionIncrement() + ":[" + cta.toString() + "]:" + oa.startOffset() + "->" + oa.endOffset() + ":" + ta.type());
+                //     System.out.println(pia.getPositionIncrement() + ":[" + cta.toString() + "]:" + oa.startOffset() + "->" + oa.endOffset() + ":" + ta.type());
+                System.out.println(cta.toString());
             }
             stream.end();
+            stream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-*/
+
 }
