9a50f7a6b1958af566d7c936459ba4753d7ecdd5
MirageLyu <1137935006@qq.com>
2019-07-12 11:15:18 +0800

    update full-featured spark-SVM

--- SparkSVM/src/main/scala/Main.scala
+++ SparkSVM/src/main/scala/Main.scala
@@ -1,47 +1,64 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
 import org.apache.spark.mllib.regression.LabeledPoint
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("SparkSVM").setMaster("local")
+  val CATEGORIES = 20
+
+  def my_compare(a: Double, b: Double) : Double = {
+    if (a == b) 1.0
+    else 0.0
+  }
+
+  def train(): Unit = {
+    val conf = new SparkConf().setAppName("SparkSVM_train").setMaster("local")
     val sc = new SparkContext(conf)
 
-    val trainRDD = MLUtils.loadLibSVMFile(sc, "hdfs://localhost:9000/cache.txt")
+    val trainRDD = MLUtils.loadLibSVMFile(sc, "hdfs://localhost:9000/train.txt")
 
-    println(trainRDD.toString())
+    for (i <- 1 to CATEGORIES) {
+      val label = i.asInstanceOf[Double]
+      val binaryTrainRDD = trainRDD.map(s => s.copy(my_compare(label, s.label), s.features))
+      val numIterations = 100
+      val model = SVMWithSGD.train(binaryTrainRDD, numIterations)
 
-    val label = 9
+      model.save(sc, "hdfs://localhost:9000/SVM_models/" + i.toString + ".model")
+    }
+  }
 
-    //trainRDD.foreach( point => if(point.label == label) point.label = 1)
+  def test(): Unit = {
+    val conf = new SparkConf().setAppName("SparkSVM_test").setMaster("local")
+    val sc = new SparkContext(conf)
 
+    val trainRDD = MLUtils.loadLibSVMFile(sc, "hdfs://localhost:9000/test.txt")
 
+    var model_list:Array[SVMModel] = new Array[SVMModel](20)
 
-    //val testRDD = MLUtils.loadLibSVMFile(sc, "hdfs://localhost:9000/data/test_data.txt")
-
-
-
-    //TODO: train N SVMs(OVR), and predict by voting.
-
-
-
-    val numIterations = 100
-    //val model = SVMWithSGD.train(trainRDD, numIterations)
-/*
-    val predictionAndLabel = testRDD.map { point =>
-      val score = model.predict(point.features)
-      (score, point.label, point.features)
+    for (i <- 1 to CATEGORIES) {
+      val model = SVMModel.load(sc, "hdfs://localhost:9000/SVM_models/" + i.toString + ".model")
+      model.clearThreshold()
+      model_list(i-1) = model
     }
 
-    val showPredict = predictionAndLabel.take(50)
-    println("Prediction" + "\t" + "Label" + "\t" + "Data")
-    for (i <- 0 to showPredict.length - 1) {
-      println(showPredict(i)._1 + "\t" + showPredict(i)._2 + "\t" + showPredict(i)._3)
-    }
+    val predict_result = trainRDD.map(point => {
+      var result = 0.0
+      var value = -1.0
+      for (i <- 1 to CATEGORIES) {
+        val re = model_list(i-1).predict(point.features)
+        if (re > value) {
+          result = i.asInstanceOf[Double]
+          value = re
+        }
+      }
+      result.asInstanceOf[Int]
+    })
 
-    // 误差计算
-    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / testRDD.count()
-    println("Accuracy = " + accuracy)*/
+    predict_result.saveAsTextFile("hdfs://localhost:9000/svm_result")
+  }
+
+  def main(args: Array[String]): Unit = {
+    //train()
+    test()
   }
 }
